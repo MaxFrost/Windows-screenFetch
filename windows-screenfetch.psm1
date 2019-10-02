ec93ff9eb64331e5c126b17ee8d357c96493b858
James Maxson <James.Maxson@QuestionMark.com>
2019-10-02 10:08:06 -0500

    Refactor and update to work on Powershell 6+ Removed line endings, reflowed to LTSB, switched to using ciminstance. Still works on Powershell 5.1 Known issues: Too many disk will break formating Art controls length of stat list, which is silly because we know the width of the art Could use some spacing off the side Could have a few other improvements Stat collection could be improved.

--- windows-screenfetch.psm1
+++ windows-screenfetch.psm1
@@ -2,59 +2,42 @@
 #### Author Julian Chow
 
 
-Function Screenfetch($distro)
-{
+Function Get-ScreenFetch {
+    [CmdletBinding()]
+    param(
+        $Distro
+    )
     $AsciiArt = "";
 
-    if (-not $distro) 
-    {
-        $AsciiArt = . Get-WindowsArt;
+    if (-not $Distro) {
+        $AsciiArt = Get-WindowsArt
     }
 
-    if (([string]::Compare($distro, "mac", $true) -eq 0) -or 
-        ([string]::Compare($distro, "macOS", $true) -eq 0) -or 
-        ([string]::Compare($distro, "osx", $true) -eq 0)) {
-            
-        $AsciiArt = . Get-MacArt;
+    if (([string]::Compare($Distro, "mac", $true) -eq 0) -or
+        ([string]::Compare($Distro, "macOS", $true) -eq 0) -or
+        ([string]::Compare($Distro, "osx", $true) -eq 0)) {
+
+        $AsciiArt = Get-MacArt
     }
-    else 
-    {
-        $AsciiArt = . Get-WindowsArt;
+    else {
+        $AsciiArt = Get-WindowsArt
     }
 
-    $SystemInfoCollection = . Get-SystemSpecifications;
-    $LineToTitleMappings = . Get-LineToTitleMappings;
+    $SystemInfoCollection = Get-SystemSpecifications
+    $LineToTitleMappings = Get-LineToTitleMappings
 
-    if ($SystemInfoCollection.Count -gt $AsciiArt.Count) 
-    { 
+    if ($SystemInfoCollection.Count -gt $AsciiArt.Count) {
         Write-Error "System Specs occupies more lines than the Ascii Art resource selected"
     }
 
-    for ($line = 0; $line -lt $AsciiArt.Count; $line++) 
-    {
-        Write-Host $AsciiArt[$line] -f Cyan -NoNewline;
-        Write-Host $LineToTitleMappings[$line] -f Red -NoNewline;
-
-        if ($line -eq 0) 
-        {
-            Write-Host $SystemInfoCollection[$line] -f Red;
+    # Need to make this agnostic of the art, but still retain the art. Needs to keep going if fields keep pushing it past art.
+    for ($line = 0; $line -lt $AsciiArt.Count; $line++) {
+        Write-Host $AsciiArt[$line] -f Cyan -NoNewline
+        if ($LineToTitleMappings[$line + 1]) {
+            Write-Host "$($LineToTitleMappings[$line + 1]): " -f Red -NoNewline
+            Write-Host $SystemInfoCollection.$($LineToTitleMappings[$line + 1]) -NoNewline
         }
-
-        elseif ($SystemInfoCollection[$line] -like '*:*') 
-        {
-            $Seperator = ":";
-            $Splitted = $SystemInfoCollection[$line].Split($seperator);
-
-            $Title = $Splitted[0] + $Seperator;
-            $Content = $Splitted[1];
-
-            Write-Host $Title -f Red -NoNewline;
-            Write-Host $Content;
-        }
-        else 
-        {
-            Write-Host $SystemInfoCollection[$line];            
-        }
+        Write-Host ""
     }
 }
 
